98a232fabf5d49d098f74448df8b6d96f7e60fae
Tomi Savolainen <tsavolai@gmail.com>
2019-10-17 22:58:05 +0300

    Change data so that mac is the key and append time to object

--- w1.py
+++ w1.py
@@ -8,8 +8,10 @@ r = redis.Redis()
 def handle_data(found_data):
     print('MAC ' + found_data[0])
     # print(found_data[1]["temperature"])
-    weather_data = {str(round(time.time())): found_data[1]}
-    r.rpush("wdata", json.dumps(weather_data))
+    found_data[1]["time"] = str(int(round(time.time())))
+    weather_data = {found_data[0]: found_data[1]}
+    r.lpush("wdata", json.dumps(weather_data))
+    r.ltrim("wdata", 0, 9)
 
 RuuviTagSensor.get_datas(handle_data)
 
